cda9b039bd06cdd360822eb7d5b1d7a3d11157e7
Jaemin <jaemin.lee.email@gmail.com>
2020-01-11 16:00:43 +0900

    Update deeplabv3.py

--- models/deeplabv3.py
+++ models/deeplabv3.py
@@ -9,29 +9,30 @@ class ASPP(nn.Module):
     def __init__(self, in_channel):
         super(ASPP, self).__init__()
         self.pool = nn.AdaptiveAvgPool2d((1,1))
-        self.conv1 = nn.Conv2d(in_channel, in_channel, kernel_size=1, padding=0, dilation=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(in_channel)
-        self.conv2 = nn.Conv2d(in_channel, in_channel, kernel_size=3, padding=6, dilation=6, bias=False)
-        self.bn2 = nn.BatchNorm2d(in_channel)
-        self.conv3 = nn.Conv2d(in_channel, in_channel, kernel_size=3, padding=12, dilation=12, bias=False)
-        self.bn3 = nn.BatchNorm2d(in_channel)
-        self.conv4 = nn.Conv2d(in_channel, in_channel, kernel_size=3, padding=18, dilation=18, bias=False)
-        self.bn4 = nn.BatchNorm2d(in_channel)
+        self.conv1 = nn.Conv2d(in_channel, 256, kernel_size=1, padding=0, dilation=1, bias=False)
+        self.conv2 = nn.Conv2d(in_channel, 256, kernel_size=1, padding=0, dilation=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.conv3 = nn.Conv2d(in_channel, 256, kernel_size=3, padding=6, dilation=6, bias=False)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.conv4 = nn.Conv2d(in_channel, 256, kernel_size=3, padding=12, dilation=12, bias=False)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.conv5 = nn.Conv2d(in_channel, 256, kernel_size=3, padding=18, dilation=18, bias=False)
+        self.bn5 = nn.BatchNorm2d(256)
 
-        self.conv5 = nn.Conv2d(in_channel * 5, in_channel, kernel_size=1, padding=0, dilation=1, bias=False)
-        self.bn5 = nn.BatchNorm2d(in_channel)
+        self.conv6 = nn.Conv2d(256 * 5, 512, kernel_size=1, padding=0, dilation=1, bias=False)
+        self.bn6 = nn.BatchNorm2d(512)
 
     def forward(self, x):
         _, _, h, w = x.size()
 
-        x1 = F.interpolate(self.pool(x), size=(h, w), mode='bilinear', align_corners=True)
-        x2 = self.bn1(self.conv1(x))
-        x3 = self.bn2(self.conv2(x))
-        x4 = self.bn3(self.conv3(x))
-        x5 = self.bn4(self.conv4(x))
+        x1 = F.interpolate(self.conv1(self.pool(x)), size=(h, w), mode='bilinear', align_corners=True)
+        x2 = self.bn2(self.conv2(x))
+        x3 = self.bn3(self.conv3(x))
+        x4 = self.bn4(self.conv5(x))
+        x5 = self.bn5(self.conv5(x))
 
         x = torch.cat((x1, x2, x3, x4, x5), 1)
-        x = self.bn5(self.conv5(x))
+        x = self.bn6(self.conv6(x))
 
         return x
    
@@ -41,7 +42,7 @@ class DeepLabV3(nn.Module):
         super(DeepLabV3, self).__init__()
         self.resnet = resnet101(pretrained=True)
         self.aspp = ASPP(2048)
-        self.conv = nn.Conv2d(2048, num_classes, kernel_size=1, padding=0)
+        self.conv = nn.Conv2d(512, num_classes, kernel_size=1, padding=0)
 
     def forward(self, x):
         x = self.resnet(x)
