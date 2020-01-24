5420fbec5cb2d9cf1776fd33877f0dfc3e3d3f7d
Jaemin <jaemin.lee.email@gmail.com>
2020-01-24 12:49:39 +0900

    revert

--- train.py
+++ train.py
@@ -79,17 +79,16 @@ def train(epoch, iteration, scheduler):
 
     print("\nepoch: ", epoch, "loss: ", train_loss/(idx+1), "lr: ", scheduler.get_lr()[0])
     
-    if rank == 0:
-        state = {
-            'net': net.module.state_dict(),
-            'epoch': epoch,
-            'iter': iteration,
-        }
+    state = {
+        'net': net.module.state_dict(),
+        'epoch': epoch,
+        'iter': iteration,
+    }
 
-        if not os.path.isdir(args.save):
-            os.makedirs(args.save)
-        saving_path = os.path.join(args.save, 'epoch' + str(epoch) + '.pth')
-        torch.save(state, saving_path)
+    if not os.path.isdir(args.save):
+        os.makedirs(args.save)
+    saving_path = os.path.join(args.save, 'epoch' + str(epoch) + '.pth')
+    torch.save(state, saving_path)
 
     return epoch, iteration
 
